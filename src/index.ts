817df8fd5b1e55c9d379ccb3326d33eaec536296
u9g <43508353+u9g@users.noreply.github.com>
2021-08-22 00:02:25 +0000

    add ts-standard

--- src/index.ts
+++ src/index.ts
@@ -3,104 +3,104 @@ import TypedEmitter from 'typed-emitter'
 import { EventEmitter } from 'events'
 
 interface MinecraftLogin {
-    username: string;
-    password?: string;
-    auth?: 'mojang' | 'microsoft';
+  username: string
+  password?: string
+  auth?: 'mojang' | 'microsoft'
 }
 
 interface ServerOptions {
-    loginHandler: (client: Client) => MinecraftLogin;
-    serverOptions?: protocolServerOptions;
-    clientOptions?: Partial<protocolClientOptions>;
+  loginHandler: (client: Client) => MinecraftLogin
+  serverOptions?: protocolServerOptions
+  clientOptions?: Partial<protocolClientOptions>
 }
 
 interface PacketMeta {
-    name: string;
+  name: string
 }
 
 interface ProxyEvents {
-    incoming: (data: any, meta: PacketMeta, toClient: ServerClient, toServer: Client) => void;
-    outgoing: (data: any, meta: PacketMeta, toClient: ServerClient, toServer: Client) => void;
+  incoming: (data: any, meta: PacketMeta, toClient: ServerClient, toServer: Client) => void
+  outgoing: (data: any, meta: PacketMeta, toClient: ServerClient, toServer: Client) => void
 }
 
 export default class ProxyHandler extends (EventEmitter as new () => TypedEmitter<ProxyEvents>) {
-    options: ServerOptions;
-    server?: Server
+  options: ServerOptions
+  server?: Server
 
-    constructor (options: ServerOptions) {
-        super()
-        this.options = options;
-        this.server = createServer({
-            'online-mode': true,
-            keepAlive: false,
-            ...this.options.serverOptions
-        })
-        this.server.on('login', client => this.onLogin(client))
-    }
+  constructor (options: ServerOptions) {
+    super()
+    this.options = options
+    this.server = createServer({
+      'online-mode': true,
+      keepAlive: false,
+      ...this.options.serverOptions
+    })
+    this.server.on('login', client => this.onLogin(client))
+  }
 
-    onLogin (toClient: ServerClient): void {
-        // until the proxyClient logs in, lets send a login packet
-        toClient.write('login', {
-            entityId: toClient.id,
-            gameMode: 0,
-            dimension: 0,
-            difficulty: 1,
-            maxPlayers: 20,
-            levelType: 'default',
-            reducedDebugInfo: false
-        })
+  onLogin (toClient: ServerClient): void {
+    // until the proxyClient logs in, lets send a login packet
+    toClient.write('login', {
+      entityId: toClient.id,
+      gameMode: 0,
+      dimension: 0,
+      difficulty: 1,
+      maxPlayers: 20,
+      levelType: 'default',
+      reducedDebugInfo: false
+    })
 
-        const toServer = createClient({
-            ...this.options.clientOptions,
-            keepAlive: false,
-            ...this.options.loginHandler(toClient)
-        })
+    const toServer = createClient({
+      ...this.options.clientOptions,
+      keepAlive: false,
+      ...this.options.loginHandler(toClient)
+    })
 
-        toServer.on('login', (data) => {
-            if (!this.clientIsOnline(toClient)) return
-            const dimension = data.dimension === 0 ? -1 : 0
-            toClient.write('respawn', {
-              dimension,
-              difficulty: data.difficulty,
-              gamemode: data.gameMode,
-              levelType: data.levelType
-            })
-            toClient.write('respawn', {
-              dimension: data.dimension,
-              difficulty: data.difficulty,
-              gamemode: data.gameMode,
-              levelType: data.levelType
-            })
-          })
+    toServer.on('login', (data) => {
+      if (!this.clientIsOnline(toClient)) return
+      const dimension = data.dimension === 0 ? -1 : 0
+      toClient.write('respawn', {
+        dimension,
+        difficulty: data.difficulty,
+        gamemode: data.gameMode,
+        levelType: data.levelType
+      })
+      toClient.write('respawn', {
+        dimension: data.dimension,
+        difficulty: data.difficulty,
+        gamemode: data.gameMode,
+        levelType: data.levelType
+      })
+    })
 
-          toClient.on('packet', (data, meta) => {
-            if (!this.clientIsOnline(toClient)) return
-            if (toServer.state === States.PLAY && meta.state === States.PLAY) {
-              this.emit('outgoing', data, meta, toClient, toServer)
-            }
-          })
+    toClient.on('packet', (data, meta) => {
+      if (!this.clientIsOnline(toClient)) return
+      if (toServer.state === States.PLAY && meta.state === States.PLAY) {
+        this.emit('outgoing', data, meta, toClient, toServer)
+      }
+    })
 
-          toServer.on('packet', (data, meta) => {
-            if (this.clientIsOnline(toClient)) return
-            if (meta.name === 'disconnect') {
-              toClient.write('kick_disconnect', data)
-            }
-            if (meta.state === States.PLAY && toClient.state === States.PLAY) {
-              if (meta.name === 'set_compression') {
-                toClient.compressionThreshold = data.threshold // Set compression
-                return
-              }
-              this.emit('incoming', data, meta, toClient, toServer)
-            }
-          })
-          toClient.once('end', () => this.clientEnd(toClient))
+    toServer.on('packet', (data, meta) => {
+      if (this.clientIsOnline(toClient)) return
+      if (meta.name === 'disconnect') {
+        toClient.write('kick_disconnect', data)
+      }
+      if (meta.state === States.PLAY && toClient.state === States.PLAY) {
+        if (meta.name === 'set_compression') {
+          toClient.compressionThreshold = data.threshold // Set compression
+          return
         }
-        
-    clientEnd (client: ServerClient): void {
-        this.server?.clients[client.id]?.end()
-    }
+        this.emit('incoming', data, meta, toClient, toServer)
+      }
+    })
+    toClient.once('end', () => this.clientEnd(toClient))
+  }
 
-    clientIsOnline (client: ServerClient): boolean {
-        return !!this.server?.clients[client.id]
-    }
+  clientEnd (client: ServerClient): void {
+    this.server?.clients[client.id]?.end()
+  }
+
+  clientIsOnline (client: ServerClient): boolean {
+    return !((this.server?.clients[client.id]) == null)
+  }
 }
